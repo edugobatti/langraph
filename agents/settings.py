7ef7a66c01e1df640478bf347108c370c9600138
Eduardo G <edugobatti@gmail.com>
2025-05-16 12:04:53 -0300

    alterações

---
+++ agents/settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+MODEL=os.getenv('MODEL')
+OPENIA_KEY=os.getenv('OPEN_AI_KEY')
+
+
--- agents/supervisor.py
+++ agents/supervisor.py
@@ -1,12 +1,23 @@
 from langgraph_supervisor import create_supervisor
 from langchain.chat_models import init_chat_model
 from agent_generator import generic_agent
+from langchain_openai import ChatOpenAI
+from settings import MODEL, OPENIA_KEY
+import json
 
-agents_create = generic_agent()
+
+caminho_arquivo = 'files/agents.json'
+
+# Abrir e carregar o conteúdo do arquivo
+with open(caminho_arquivo, 'r', encoding='utf-8') as f:
+    dados = json.load(f)
+
+
+agents_create = generic_agent(dados)
 
 
 supervisor = create_supervisor(
-    model=llm,
+    model=ChatOpenAI(model=MODEL, api_key=OPENIA_KEY),
     agents=list(agents_create.values()),
     prompt=(
         "Você é um supervisor gerenciando agentes:\n"
@@ -14,5 +25,7 @@ supervisor = create_supervisor(
         "Não faça nenhum trabalho sozinho."
     ),
     add_handoff_back_messages=True,
-    output_mode="full_history",
+    output_mode="last_message",
 ).compile()
+
+print('Quanto é 2+2?')
